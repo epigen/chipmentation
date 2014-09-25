697acb734374c98f99640395b06ce4b1e68827af
André Rendeiro <arendeiro@cemm.oeaw.ac.at>
2014-09-25 10:27:51 +0200

    added awk code to shift reads from tag samples

--- src/shift_reads.sh
+++ src/shift_reads.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --partition=shortq
+#SBATCH --partition=develop
 #SBATCH --ntasks=1
 #SBATCH --time=10:00:00
 
-#SBATCH --cpus-per-task=16
-#SBATCH --mem-per-cpu=2000
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4000
 #SBATCH --nodes=1
 
-#SBATCH --job-name=mapping
-#SBATCH --output=/home/arendeiro/logs/mapping_%j.out
-#SBATCH --error=/home/arendeiro/logs/mapping_%j.err
+#SBATCH --job-name=shiftReads
+#SBATCH --output=/home/arendeiro/logs/shiftReads_%j.out
+#SBATCH --error=/home/arendeiro/logs/shiftReads_%j.err
 
 
 ####### EXPLANATION OF USAGE ##########
@@ -64,28 +64,6 @@ PROJECTDIR=/home/arendeiro/data/human/chipmentation
 GENOMEREF=/fhgfs/prod/ngs_resources/genomes/hg19/forBowtie2/hg19
 PRESEQ=/home/arendeiro/.local/software/preseq-0.1.0/preseq
 
-### Start work on samples 
-# Fastqc on raw data
-mkdir -p $PROJECTDIR/raw/fastqc
-fastqc -o $PROJECTDIR/raw/fastqc $SAMPLE_FILE
-
-# Convert to fastq
-mkdir -p $PROJECTDIR/raw/fastq
-bamtools convert -in $SAMPLE_FILE -format fastq > $PROJECTDIR/raw/fastq/$SAMPLE_NAME.fastq
-
-# Trimming 
-# Remove adapters, trim reads when needed
-java -jar `which trimmomatic-0.32.jar` SE \
-$PROJECTDIR/raw/fastq/$SAMPLE_NAME.fastq ${PROJECTDIR}/raw/${SAMPLE_NAME}.trimmed.fastq \
-ILLUMINACLIP:/home/arendeiro/adapters/TruSeq3-SE.fa:1:40:15 \
-LEADING:3 TRAILING:3 \
-SLIDINGWINDOW:4:10 \
-MINLEN:36
-
-# Map with bowtie2 end-to-end sensitive settings
-mkdir -p $PROJECTDIR/mapped/
-bowtie2 --very-sensitive -p 16 -x $GENOMEREF $PROJECTDIR/raw/$SAMPLE_NAME.trimmed.fastq | samtools view -S -b - | samtools sort - $PROJECTDIR/mapped/$SAMPLE_NAME.trimmed.bowtie2.sorted
-
 # Shift reads from tagmentation samples
  if [[ $SAMPLE_NAME == *ATAC-seq* ]] || [[ $SAMPLE_NAME == *CM* ]]
     then
@@ -94,19 +72,9 @@ bowtie2 --very-sensitive -p 16 -x $GENOMEREF $PROJECTDIR/raw/$SAMPLE_NAME.trimme
     samtools view $PROJECTDIR/mapped/$SAMPLE_NAME.trimmed.bowtie2.sorted.bam | awk -F "\t" 'BEGIN{OFS="\t"}($2==0){ offset=$4+4; print $1,$2,$3, offset,$5,$6,$7,$8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20}' > $PROJECTDIR/mapped/$SAMPLE_NAME.trimmed.bowtie2.sorted.shifted_plus.sam
     cat $PROJECTDIR/mapped/$SAMPLE_NAME.trimmed.bowtie2.sorted.shifted_minus.sam $PROJECTDIR/mapped/$SAMPLE_NAME.trimmed.bowtie2.sorted.shifted_plus.sam | samtools view -S -b - | samtools sort - $PROJECTDIR/mapped/$SAMPLE_NAME.trimmed.bowtie2.sorted.shifted.bam
     if [ -s $PROJECTDIR/mapped/$SAMPLE_NAME.trimmed.bowtie2.sorted.shifted.bam ]
-        then
+    	then
         rm $PROJECTDIR/mapped/$SAMPLE_NAME.trimmed.bowtie2.sorted.shifted_minus.sam $PROJECTDIR/mapped/$SAMPLE_NAME.trimmed.bowtie2.sorted.shifted_plus.sam
     fi
-fi
-
-# Preseq
-if [[ $SAMPLE_NAME == *ATAC-seq* || $SAMPLE_NAME == *CM*]]
-    then
-    $PRESEQ c_curve -B $PROJECTDIR/mapped/$SAMPLE_NAME.trimmed.bowtie2.sorted.shifted.bam -o $PROJECTDIR/mapped/$SAMPLE_NAME_qc_c_curve.txt
-    $PRESEQ c_curve -B $PROJECTDIR/mapped/$SAMPLE_NAME.trimmed.bowtie2.sorted.shifted.bam -e 1e8 -s 2e6 -o $PROJECTDIR/mapped/$SAMPLE_NAME_qc_lc_extrap.txt
-else
-    $PRESEQ c_curve -B $PROJECTDIR/mapped/$SAMPLE_NAME.trimmed.bowtie2.sorted.bam -o $PROJECTDIR/mapped/$SAMPLE_NAME_qc_c_curve.txt
-    $PRESEQ c_curve -B $PROJECTDIR/mapped/$SAMPLE_NAME.trimmed.bowtie2.sorted.bam -e 1e8 -s 2e6 -o $PROJECTDIR/mapped/$SAMPLE_NAME_qc_lc_extrap.txt
 fi
 
 date
--- src/testsubmit.sh
+++ src/testsubmit.sh
@@ -3,10 +3,13 @@
 #SBATCH --ntasks=1
 #SBATCH --time=10:00:00
 
-# Optional parameters
-#SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=2000
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1000
 #SBATCH --nodes=1
+
+#SBATCH --job-name=test%j
+#SBATCH --output=/home/arendeiro/logs/test%j.out
+#SBATCH --error=/home/arendeiro/logs/test%j.err
 #SBATCH --mail-type=end
 #SBATCH --mail-user=arendeiro@cemm.oeaw.ac.at
 
@@ -30,11 +33,11 @@
 #
 # $ while read SAMPLE_NAME SAMPLE_FILE; do
 # $    sbatch /home/arendeiro/projects/chipmentation/src/pipeline_with_trimming.sh \
-# $    $SAMPLE_NAME \
-# $    $SAMPLE_FILE \
 # $    --job-name="pipeline_trim_${SAMPLE_NAME}" \
 # $    --error="/home/arendeiro/logs/pipeline_trim_${SAMPLE_NAME}_%j.err" \
 # $    --output="/home/arendeiro/logs/pipeline_trim_${SAMPLE_NAME}_%j.log" \
+# $    $SAMPLE_NAME \
+# $    $SAMPLE_FILE
 # $ done < $SAMPLES_FILE
 
 
@@ -45,7 +48,6 @@ module load trimmomatic/0.32
 module load samtools
 module load bamtools/bamtools
 module load bowtie/2.2.0
-module load picard-tools/1.118
 
 # set the temporary folder to your scratch location (avoid using node local /tmp)
 export SCRATCH_PATH=/fhgfs/scratch/users/arendeiro
@@ -55,45 +57,10 @@ export TMPDIR=$SCRATCH_PATH/tmp
 hostname
 date
 
-### Get sample info from arguments
-SAMPLE_NAME=$1
-SAMPLE_FILE=$2
-CONTROL_NAME=$3
-CONTROL_FILE=$4
-
-### Specify paths
-PROJECTDIR=/home/arendeiro/data/human/chipmentation
-GENOMEREF=/fhgfs/prod/ngs_resources/genomes/hg19/forBowtie2/hg19
-PICARDDIR=/cm/shared/apps/picard-tools/1.118
-PRESEQ=/home/arendeiro/.local/software/preseq-0.1.0/preseq
-MACS2="python2.7 .local/software/bin/macs2"
-HOMERDIR=/home/arendeiro/.local/software/homer-4.6/bin
-
-### Call Peaks
-mkdir -p $PROJECTDIR/trimmed/peaks/
-
-# MACS
-$MACS2 callpeak -t $SAMPLE_FILE \
--c $CONTROL_FILE \
--g hs -n $SAMPLE_NAME --outdir $PROJECTDIR/trimmed/peaks/${SAMPLE_NAME}_peaks_MACS2
-
-# HOMER
-# make tag dirs
-mkdir -p $PROJECTDIR/homer/${SAMPLE_NAME}_homer
-$HOMERDIR/makeTagDirectory $PROJECTDIR/homer/${SAMPLE_NAME}_homer $SAMPLE_FILE
-$HOMERDIR/makeTagDirectory $PROJECTDIR/homer/${CONTROL_NAME}_homer $CONTROL_FILE
-# make UCSC tracks
-$HOMERDIR/makeUCSCfile $PROJECTDIR/homer/${SAMPLE_NAME}_homer -name $SAMPLE_NAME -o auto
-$HOMERDIR/makeUCSCfile $PROJECTDIR/homer/${CONTROL_NAME}_homer -name $CONTROL_NAME -o auto
-
-# find peaks
-if [[ $SAMPLE_NAME == *H3* ]]
-    then
-    echo "Detected histone data"
-    $HOMERDIR/findPeaks $PROJECTDIR/homer/${SAMPLE_NAME}_homer -style histone -o auto -i $PROJECTDIR/homer/${CONTROL_NAME}_homer
-else
-    echo "Detected TF data"
-    $HOMERDIR/findPeaks $PROJECTDIR/homer/${SAMPLE_NAME}_homer -style factor -o auto -i $PROJECTDIR/homer/${CONTROL_NAME}_homer
-fi
+echo $1
+echo $2
+echo $3
+echo $4
+echo $5
 
 date
