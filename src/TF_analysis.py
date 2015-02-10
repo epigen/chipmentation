3a9ee06bffe9653d0c94d8a1d24bfe13979fc2cd
André Rendeiro <afrendeiro@gmail.com>
2015-02-10 12:07:29 +0100

    investigated differences between CTCF sites in chromatin loops and outside loops

--- src/TF_analysis.py
+++ src/TF_analysis.py
@@ -16,6 +16,8 @@ import pybedtools
 import numpy as np
 import pandas as pd
 
+from divideAndSlurm import DivideAndSlurm, Task
+
 from ggplot import ggplot, aes, stat_smooth, facet_wrap, xlab, ylab, theme_bw, theme
 
 import rpy2.robjects as robj # for ggplot in R
@@ -48,6 +50,131 @@ plotly.sign_in("afrendeiro", "iixmygxac1")
 windowWidth = abs(windowRange[0]) + abs(windowRange[1])
 
 
+
+class Coverage(Task):
+    """
+    Task to get read coverage under regions.
+    """
+    def __init__(self, data, fractions, *args, **kwargs):
+        import string, time, random, textwrap
+        super(Coverage, self).__init__(data, fractions, *args, **kwargs)
+        ### Initialize rest
+        now = string.join([time.strftime("%Y%m%d%H%M%S", time.localtime()), str(random.randint(1,1000))], sep="_")
+        self.name = "coverage_{0}".format(now)
+
+        ### Parse
+        # required argument
+        if len(self.args) != 1:
+            raise TypeError("Bam file argument is missing")
+        self.bam_file = self.args[0]
+        # additional arguments
+        if "strand_wise" in kwargs.keys():
+            self.strand_wise = kwargs["strand_wise"]
+        else:
+            self.strand_wise = True
+        if "duplicates" in kwargs.keys():
+            self.duplicates = kwargs["duplicates"]
+        else:
+            self.duplicates = True
+        if "orientation" in kwargs.keys():
+            self.orientation = kwargs["orientation"]
+        else:
+            self.orientation = False
+        if "permute" in kwargs.keys():
+            self.permute = kwargs["permute"]
+        else:
+            self.permute = False
+        if "fragment_size" in kwargs.keys():
+            self.fragment_size = kwargs["fragment_size"]
+        else:
+            self.fragment_size = 1
+
+    def _prepare(self):
+        """
+        Add task to be performed with data. Is called when task is added to DivideAndSlurm object.
+        """        
+        self.log = os.path.join(self.slurm.logDir, string.join([self.name, "log"], sep=".")) # add abspath
+
+        ### Split data in fractions
+        ids, groups, files = self._split_data()
+
+        ### Make jobs with groups of data
+        self.jobs = list(); self.jobFiles = list(); self.inputPickles = list(); self.outputPickles = list()
+
+        # for each group of data
+        for i in xrange(len(ids)):
+            jobFile = files[i] + "_coverage.sh"
+            inputPickle = files[i] + ".input.pickle"
+            outputPickle = files[i] + ".output.pickle"
+
+            ### assemble job file
+            # header
+            job = self._slurmHeader(ids[i])
+
+            # command - add abspath!
+            task = """\
+                # Activate virtual environment
+                source /home/arendeiro/venv/bin/activate
+
+                python /home/arendeiro/coverage_parallel.py {0} {1} {2} """.format(inputPickle, outputPickle, self.bam_file)
+
+            if self.strand_wise:
+                task += "--strand-wise "
+            if self.duplicates:
+                task += "--duplicates "
+            if self.permute:
+                task += "--permute "
+            task += "--fragment-size {0}".format(self.fragment_size)
+
+            task += """
+
+                # Deactivate virtual environment
+                deactivate
+                    """
+
+            job += textwrap.dedent(task)
+
+            # footer
+            job += self._slurmFooter()
+
+            # add to save attributes
+            self.jobs.append(job)
+            self.jobFiles.append(jobFile)
+            self.inputPickles.append(inputPickle)
+            self.outputPickles.append(outputPickle)
+
+            # write job file to disk
+            with open(jobFile, 'w') as handle:
+                handle.write(textwrap.dedent(job))
+
+        # Delete data if jobs are ready to submit and data is serialized
+        if hasattr(self, "jobs") and hasattr(self, "jobFiles"):
+            del self.data
+
+    def collect(self):
+        """
+        If self.is_ready(), return joined reduced data.
+        """
+        if hasattr(self, "output"): # if output is already stored, just return it
+            return self.output
+
+        if self.is_ready():
+            # load all pickles into list
+            if self.permissive:
+                outputs = [pickle.load(open(outputPickle, 'r')) for outputPickle in self.outputPickles if os.path.isfile(outputPickle)]
+            else:
+                outputs = [pickle.load(open(outputPickle, 'r')) for outputPickle in self.outputPickles]
+            # if all are counters, and their elements are counters, sum them
+            if all([type(outputs[i]) == dict for i in range(len(outputs))]):
+                output = reduce(lambda x, y: dict(x, **y), outputs)
+                if type(output) == dict:
+                    self.output = output # store output in object
+                    self._rm_temps() # delete tmp files
+                    return self.output
+        else:
+            raise TypeError("Task is not ready yet.")
+
+
 def bedToolsInterval2GenomicInterval(bedtool):
     """
     Given a pybedtools.BedTool object, returns dictionary of HTSeq.GenomicInterval objects.
@@ -61,67 +188,6 @@ def bedToolsInterval2GenomicInterval(bedtool):
         else:
             intervals[iv.name] = HTSeq.GenomicInterval(iv.chrom, iv.start, iv.end)
     return intervals
-
-
-def coverage(bam, intervals, fragmentsize, orientation=True, duplicates=True, strand_specific=False):
-    """
-    Gets read coverage in bed regions.
-    Returns dict of regionName:numpy.array if strand_specific=False, A dict of "+" and "-" keys with regionName:numpy.array
-    bam - HTSeq.BAM_Reader object. Must be sorted and indexed with .bai file!
-    intervals - dict with HTSeq.GenomicInterval objects as values
-    fragmentsize - integer
-    stranded - boolean
-    duplicates - boolean.
-    """
-    # Loop through TSSs, get coverage, append to dict
-    cov = OrderedDict()
-    i=0
-    for name, feature in intervals.iteritems():
-        if i%1000 == 0:
-            print(len(intervals) - i)
-        # Initialize empty array for this feature
-        if not strand_specific:
-            profile = np.zeros(feature.length, dtype=np.int8)
-        else:
-            profile = np.zeros((2, feature.length), dtype=np.int8)
-                    
-        # Fetch alignments in feature window
-        for aln in bam[feature]:
-            # check if duplicate
-            if not duplicates and aln.pcr_or_optical_duplicate:
-                continue
-
-            aln.iv.length = fragmentsize # adjust to size
-            
-            # get position in relative to window
-            if orientation:
-                if feature.strand == "+" or feature.strand == ".":
-                    start_in_window = aln.iv.start - feature.start - 1
-                    end_in_window   = aln.iv.end   - feature.start - 1
-                else:
-                    start_in_window = feature.length - abs(feature.start - aln.iv.end) - 1
-                    end_in_window   = feature.length - abs(feature.start - aln.iv.start) - 1
-            else:
-                start_in_window = aln.iv.start - feature.start - 1
-                end_in_window   = aln.iv.end   - feature.start - 1
-            
-            # check fragment is within window; this is because of fragmentsize adjustment
-            if start_in_window < 0 or end_in_window > feature.length:
-                continue
-
-            # add +1 to all positions overlapped by read within window
-            if not strand_specific:
-                profile[start_in_window : end_in_window] += 1
-            else:
-                if aln.iv.strand == "+":
-                    profile[0][start_in_window : end_in_window] += 1
-                elif aln.iv.strand == "-":
-                    profile[1][start_in_window : end_in_window] += 1
-        
-        # append feature profile to dict
-        cov[name] = profile
-        i+=1
-    return cov
 
 
 def exportToJavaTreeView(df, filename):
@@ -139,6 +205,9 @@ def exportToJavaTreeView(df, filename):
     df.to_csv(filename, sep="\t", index=False)
 
 
+
+slurm = DivideAndSlurm()
+
 # Loop through all samples, compute coverage in peak regions centered on motifs,
 # save dicts with coverage and average profiles
 rawSignals = OrderedDict()
@@ -150,26 +219,22 @@ for signal in signals.keys():
     # peaks = pybedtools.BedTool(os.path.join(peakFilePath, signal + ".motifStrand.bed")).slop(genome=genome, b=windowWidth/2)
     peaks = pybedtools.BedTool(os.path.join(peakFilePath, signal + ".motifStrand.bed"))
     peaks = bedToolsInterval2GenomicInterval(peaks)
-        # Filter peaks near chrm borders
+    # Filter peaks near chrm borders
     for name, interval in peaks.iteritems():
         if interval.length < windowWidth:
             peaks.pop(name)
 
-    # Subset
-    # i=0
-    # d = dict()
-    # for name, peak in peaks.iteritems():
-    #     if i < 500:
-    #         d[name] = peak
-    #         i+=1
-    # peaks = d
-
     for sample in signals[signal]:
-        # Load bam
-        bamfile = HTSeq.BAM_Reader(os.path.join(bamFilePath, sample + ".bam"))
-
         # Get dataframe of signal coverage in bed regions, append to dict
-        cov = coverage(bamfile, peaks, fragmentsize, strand_specific=True)
+        # Make task
+        task = Coverage(peaks, 30, os.path.join(bamFilePath, sample + ".bam"),
+            orientation=True, fragment_size=1, strand_specific=False, queue="shortq", cpusPerTask=8
+        )
+        slurm.add_task(task)
+        slurm.submit(task)
+        while not task.is_ready():
+            time.sleep(10)
+        cov = task.collect()
 
         # Make multiindex dataframe
         levels = [cov.keys(), ["+", "-"]]
@@ -198,16 +263,15 @@ for signal in signals.keys():
         }
         aveSignals[sample] = pd.DataFrame(ave)
 
-
 # serialize with pickle
-with open(os.path.join(coverageFilePath, "tssSignals_{0}bp.rawSignals.pickl".format(str(windowWidth))), 'wb') as handle:
+with open(os.path.join(coverageFilePath, "tssSignals_{0}bp.rawSignals.pickle".format(str(windowWidth))), 'wb') as handle:
     pickle.dump(rawSignals, handle, protocol=pickle.HIGHEST_PROTOCOL)
-# with open(os.path.join(coverageFilePath, "tssSignals_{0}bp.rawSignals.pickl".format(str(windowWidth))), 'rb') as handle:
+# with open(os.path.join(coverageFilePath, "tssSignals_{0}bp.rawSignals.pickle".format(str(windowWidth))), 'rb') as handle:
 #     rawSignals = pickle.load(handle)
 
-with open(os.path.join(coverageFilePath, "tssSignals_{0}bp.aveSignals.pickl".format(str(windowWidth))), 'wb') as handle:
+with open(os.path.join(coverageFilePath, "tssSignals_{0}bp.aveSignals.pickle".format(str(windowWidth))), 'wb') as handle:
     pickle.dump(aveSignals, handle, protocol=pickle.HIGHEST_PROTOCOL)
-# with open(os.path.join(coverageFilePath, "tssSignals_{0}bp.aveSignals.pickl".format(str(windowWidth))), 'rb') as handle:
+# with open(os.path.join(coverageFilePath, "tssSignals_{0}bp.aveSignals.pickle".format(str(windowWidth))), 'rb') as handle:
 #     aveSignals = pickle.load(handle)
 
 # Plot average profiles for all signals with ggplot
@@ -283,3 +347,91 @@ for signal in signals:
     
     # Export as cdt
     exportToJavaTreeView(df, os.path.join(plotsDir, exportName + ".cdt"))
+
+
+
+
+
+
+"""
+Investigating the contribution of chromatin loops to TF ChIPmentation signal
+"""
+
+signal = "CTCF_K562_10mio_CM"
+sample = "CTCF_K562_10mio_CM"
+
+# load ChIPmentation peaks
+peaks = pybedtools.BedTool(os.path.join(peakFilePath, signal + ".motifStrand.bed"))
+
+# intersect with chromatin loops
+loops = pybedtools.BedTool("reference/Homo_sapiens/Hi-C/GSE63525_K562_HiCCUPS_looplist.concat.txt")
+
+peaks_in_loops = peaks.intersect(b=loops, wa=True)
+peaks_notin_loops = peaks.intersect(b=loops, v=True, wa=True)
+
+peaks_in_loops = bedToolsInterval2GenomicInterval(peaks_in_loops)
+peaks_notin_loops = bedToolsInterval2GenomicInterval(peaks_notin_loops)
+
+
+for name, interval in peaks_in_loops.iteritems():
+        if interval.length < windowWidth:
+            peaks_in_loops.pop(name)
+for name, interval in peaks_notin_loops.iteritems():
+        if interval.length < windowWidth:
+            peaks_notin_loops.pop(name)
+
+loopsTask = Coverage(peaks_in_loops, 30, os.path.join(bamFilePath, sample + ".bam"),
+    orientation=True, fragment_size=1, strand_specific=False, queue="shortq", cpusPerTask=2
+)
+notLoopsTask = Coverage(peaks_notin_loops, 30, os.path.join(bamFilePath, sample + ".bam"),
+    orientation=True, fragment_size=1, strand_specific=False, queue="shortq", cpusPerTask=2
+)
+slurm.add_task(loopsTask)
+slurm.add_task(notLoopsTask)
+
+slurm.submit(loopsTask)
+slurm.submit(notLoopsTask)
+
+loopsCov = loopsTask.collect()
+notLoopsCov = notLoopsTask.collect()
+
+levels = [loopsCov.keys(), ["+", "-"]]
+labels = [[y for x in range(len(loopsCov)) for y in [x, x]], [y for x in range(len(loopsCov.keys())) for y in (0,1)]]
+index = pd.MultiIndex(labels=labels, levels=levels, names=["peak", "strand"])
+loopsDf = pd.DataFrame(np.vstack(loopsCov.values()), index=index)
+loopsDf.columns = range(windowRange[0], windowRange[1])
+
+levels = [notLoopsCov.keys(), ["+", "-"]]
+labels = [[y for x in range(len(notLoopsCov)) for y in [x, x]], [y for x in range(len(notLoopsCov.keys())) for y in (0,1)]]
+index = pd.MultiIndex(labels=labels, levels=levels, names=["peak", "strand"])
+notLoopsDf = pd.DataFrame(np.vstack(notLoopsCov.values()), index=index)
+notLoopsDf.columns = range(windowRange[0], windowRange[1])
+
+# Save as csv
+loopsDf.to_csv(os.path.join(coverageFilePath, "{0}_inLoops.tssSignal_{1}bp.csv".format(sample, str(windowWidth))), index=False)
+notLoopsDf.to_csv(os.path.join(coverageFilePath, "{0}_notinLoops.tssSignal_{1}bp.csv".format(sample, str(windowWidth))), index=False)
+# loopsDf = pd.read_csv(os.path.join(coverageFilePath, "{0}_inLoops.tssSignal_{1}bp.csv".format(sample, str(windowWidth))))
+# notLoopsDf = pd.read_csv(os.path.join(coverageFilePath, "{0}_notinLoops.tssSignal_{1}bp.csv".format(sample, str(windowWidth))))
+
+# Pickle
+pickle.dump(loopsDf, open(os.path.join(coverageFilePath, "{0}_inLoops.tssSignal_{1}bp.pickle".format(sample, str(windowWidth))), "wb"), protocol=pickle.HIGHEST_PROTOCOL)
+pickle.dump(notLoopsDf, open(os.path.join(coverageFilePath, "{0}_notinLoops.tssSignal_{1}bp.pickle".format(sample, str(windowWidth))), "wb"), protocol=pickle.HIGHEST_PROTOCOL)
+# loopsDf = pickle.load(open(os.path.join(coverageFilePath, "{0}_inLoops.tssSignal_{1}bp.pickle".format(sample, str(windowWidth))), "r"))
+# notLoopsDf = pickle.load(open(os.path.join(coverageFilePath, "{0}_notinLoops.tssSignal_{1}bp.pickle".format(sample, str(windowWidth))), "r"))
+
+
+# Get average profiles and append to dict
+loopsAve = {
+    "signal" : sample,
+    "average" : loopsDf.apply(np.mean, axis=0),                              # both strands
+    "positive" : loopsDf.ix[range(0, len(loopsDf), 2)].apply(np.mean, axis=0),    # positive strand
+    "negative" : loopsDf.ix[range(1, len(loopsDf), 2)].apply(np.mean, axis=0)     # negative strand
+}
+notLoopsAve = {
+    "signal" : sample,
+    "average" : notLoopsDf.apply(np.mean, axis=0),                              # both strands
+    "positive" : notLoopsDf.ix[range(0, len(notLoopsDf), 2)].apply(np.mean, axis=0),    # positive strand
+    "negative" : notLoopsDf.ix[range(1, len(notLoopsDf), 2)].apply(np.mean, axis=0)     # negative strand
+}
+pickle.dump(loopsAve, open(os.path.join(coverageFilePath, "{0}_inLoops_average.tssSignal_{1}bp.pickle".format(sample, str(windowWidth))), "wb"), protocol=pickle.HIGHEST_PROTOCOL)
+pickle.dump(notLoopsDf, open(os.path.join(coverageFilePath, "{0}_notinLoops_average.tssSignal_{1}bp.pickle".format(sample, str(windowWidth))), "wb"), protocol=pickle.HIGHEST_PROTOCOL)
