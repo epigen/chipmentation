c5623fe9c74ccb3f0a5ffe243906e9aa1d4e39b1
André Rendeiro <afrendeiro@gmail.com>
2015-03-10 20:28:07 +0100

    added histone CM data on TF peaks - TF footprints visible

--- src/TF_analysis.py
+++ src/TF_analysis.py
@@ -11,10 +11,12 @@
 
 import os
 from collections import OrderedDict
+import re
 import HTSeq
 import pybedtools
 import numpy as np
 import pandas as pd
+import gc
 
 from divideAndSlurm import DivideAndSlurm, Task
 import string, time, random, textwrap
@@ -28,27 +30,115 @@ import rpy2.robjects as robj # for ggplot in R
 import rpy2.robjects.pandas2ri # for R dataframe conversion
 from rpy2.robjects.packages import importr
 
-from sklearn.cluster import k_means 
+from sklearn.cluster import k_means
 import cPickle as pickle
 
 import plotly.plotly as plotly
 from plotly.graph_objs import Data, Heatmap, Layout, Figure
 
-signals = {"CTCF_K562_10mio_CM": ["CTCF_K562_10mio_CM", "CTCF_K562_10mio_ChIP", "IgG_K562_10mio_CM"],
-           "PU1_K562_10mio_CM": ["PU1_K562_10mio_CM", "PU1_K562_10mio_ChIP", "IgG_K562_10mio_CM"]}  # DNase missing
-bamFilePath = "/home/arendeiro/data/human/chipmentation/mapped/merged"
-peakFilePath = "/home/arendeiro/data/human/chipmentation/bed"
-coverageFilePath = "/home/arendeiro/data/human/chipmentation/bed/newPython"  # new output dir
-plotsDir = "/home/arendeiro/"
+import numpy.lib
+import numpy as np
+import pandas as pd
+import cPickle as pickle
 
-genome = "hg19"
-windowRange = (-2000, 2000)
-fragmentsize = 1
-duplicates = True
-n_clusters = 5
 
-plotly.sign_in("afrendeiro", "iixmygxac1")
-windowWidth = abs(windowRange[0]) + abs(windowRange[1])
+def smooth(x, window_len=11, window='hanning'):
+    """smooth the data using a window with requested size.
+
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+
+    input:
+        x: the input signal
+        window_len: the dimension of the smoothing window; should be an odd integer
+        window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
+            flat window will produce a moving average smoothing.
+
+    output:
+        the smoothed signal
+
+    example:
+
+    t=linspace(-2,2,0.1)
+    x=sin(t)+randn(len(t))*0.1
+    y=smooth(x)
+
+    see also:
+
+    numpy.hanning, numpy.hamming, numpy.bartlett, numpy.blackman, numpy.convolve
+    scipy.signal.lfilter
+
+    TODO: the window parameter could be the window itself if an array instead of a string
+    NOTE: length(output) != length(input), to correct this: return y[(window_len/2-1):-(window_len/2)] instead of just y.
+    """
+    if x.ndim != 1:
+        raise ValueError("smooth only accepts 1 dimension arrays.")
+
+    if x.size < window_len:
+        raise ValueError("Input vector needs to be bigger than window size.")
+
+    if window_len < 3:
+        return x
+
+    if window not in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError("Window is one of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'")
+
+    s = numpy.r_[x[window_len - 1:0:-1], x, x[-1:-window_len:-1]]
+    # print(len(s))
+    if window == 'flat':  # moving average
+        w = numpy.ones(window_len, 'd')
+    else:
+        w = eval('numpy.' + window + '(window_len)')
+
+    y = numpy.convolve(w / w.sum(), s, mode='valid')
+    return y
+
+
+def savePandas(fname, data):
+    '''Save DataFrame or Series
+
+    Parameters
+    ----------
+    fname : str
+        filename to use
+    data: Pandas DataFrame or Series
+    '''
+    np.save(open(fname, 'w'), data)
+    if len(data.shape) == 2:
+        meta = data.index, data.columns
+    elif len(data.shape) == 1:
+        meta = (data.index,)
+    else:
+        raise ValueError('save_pandas: Cannot save this type')
+    s = pickle.dumps(meta)
+    s = s.encode('string_escape')
+    with open(fname, 'a') as f:
+        f.seek(0, 2)
+        f.write(s)
+
+
+def loadPandas(fname, mmap_mode='r'):
+    '''Load DataFrame or Series
+
+    Parameters
+    ----------
+    fname : str
+        filename
+    mmap_mode : str, optional
+        Same as numpy.load option
+    '''
+    values = np.load(fname, mmap_mode=mmap_mode)
+    with open(fname) as f:
+        numpy.lib.format.read_magic(f)
+        numpy.lib.format.read_array_header_1_0(f)
+        f.seek(values.dtype.alignment * values.size, 1)
+        meta = pickle.loads(f.readline().decode('string_escape'))
+    if len(meta) == 2:
+        return pd.DataFrame(values, index=meta[0], columns=meta[1])
+    elif len(meta) == 1:
+        return pd.Series(values, index=meta[0]).copy()
 
 
 class Coverage(Task):
@@ -206,98 +296,190 @@ def exportToJavaTreeView(df, filename):
     df.to_csv(filename, sep="\t", index=False)
 
 
+# Define paths
+projectRoot = "/fhgfs/groups/lab_bock/shared/projects/cm/"
+resultsDir = projectRoot + "results"
+plotsDir = resultsDir + "/plots"
+bedFilePath = "/home/arendeiro/reference/Homo_sapiens/hg19.cage_peak_coord_robust.TATA_Annotated.bed"
+
+# Get samples
+samples = pd.read_csv(os.path.abspath(projectRoot + "cm.replicates.annotation_sheet.csv"))
+
+# subset samples
+sampleSubset = samples[samples['sampleName'].str.contains(
+    "K562_10M_CM_GATA1_nan_nan_1_0_hg19|" +
+    "K562_10M_CM_PU1_nan_nan_0_0_hg19|" +
+    "K562_10M_CM_REST_nan_nan_1_0_hg19"
+)].reset_index(drop=True)
+
+# subset samples into signals
+signals = samples[samples['sampleName'].str.contains(
+    "K562_10M_CHIP_H3K27AC_nan_nan_1_0_hg19|" +
+    "K562_10M_CHIP_H3K4ME1_nan_nan_1_0_hg19|" +
+    "K562_10M_CM_H3K27AC_nan_nan_1_0_hg19|" +
+    "K562_10M_CM_H3K4ME1_nan_nan_1_0_hg19|" +
+    "K562_500K_CHIP_H3K27ME3_nan_nan_0_0_hg19|" +
+    "K562_500K_CHIP_H3K4ME3_nan_nan_0_0_hg19|" +
+    "K562_500K_CM_H3K27ME3_nan_nan_0_0_hg19|" +
+    "K562_500K_CM_H3K4ME3_nan_nan_0_0_hg19"
+)].reset_index(drop=True)
+
+genome = "hg19"
+windowRange = (-1000, 1000)
+fragmentsize = 1
+duplicates = False
+n_clusters = 5
+
+plotly.sign_in("afrendeiro", "iixmygxac1")
+windowWidth = abs(windowRange[0]) + abs(windowRange[1])
+
+
 slurm = DivideAndSlurm()
 
-# Loop through all samples, compute coverage in peak regions centered on motifs,
-# save dicts with coverage and average profiles
-rawSignals = OrderedDict()
-aveSignals = OrderedDict()
-for signal in signals.keys():
-    # Load peak file from bed files, centered on motif
+gapsRepeats = pybedtools.BedTool("/home/arendeiro/reference/Homo_sapiens/hg19_gapsRepeats.bed")
 
-    # TODO: modify pipeline so that slop is done here
-    # peaks = pybedtools.BedTool(os.path.join(peakFilePath, signal + ".motifStrand.bed")).slop(genome=genome, b=windowWidth/2)
-    peaks = pybedtools.BedTool(os.path.join(peakFilePath, signal + ".motifStrand.bed"))
+# Loop through all samples, submit tasks to compute coverage in peak regions centered on motifs
+for i in range(len(sampleSubset)):
+    sampleName = sampleSubset['sampleName'][i]
+
+    motifCentered = re.sub("\..*", ".", sampleSubset['peakFile'][i]) + "motifCentered.bed"
+    # Load peak file from bed files centered on motif, make window around
+    try:
+        peaks = pybedtools.BedTool(motifCentered)  # .slop(genome=genome, b=windowWidth / 2)
+    except IOError("File not found"), e:
+        raise e
+
+    # exlude peaks in gaps or repeats
+    peaks.intersect(b=gapsRepeats, v=True, wa=True)
+
     peaks = bedToolsInterval2GenomicInterval(peaks)
     # Filter peaks near chrm borders
-    for name, interval in peaks.iteritems():
+    for peak, interval in peaks.iteritems():
         if interval.length < windowWidth:
-            peaks.pop(name)
+            peaks.pop(peak)
 
-    for sample in signals[signal]:
+    # Get self coverage
+    signalName = sampleName
+    exportName = "-".join([sampleName, signalName])
+    bamFile = sampleSubset['filePath'][i]
+
+    task = Coverage(peaks, 2, os.path.join(bamFile),
+                    orientation=True, fragment_size=1, strand_wise=True, queue="shortq", cpusPerTask=8
+                    )
+    task.id = exportName
+    slurm.add_task(task)
+    slurm.submit(task)
+
+    # Get coverage over other signals
+    for j in range(len(signals)):
+        signalName = signals['sampleName'][j]
+
+        exportName = "-".join([sampleName, signalName])
+        if os.path.isfile(os.path.join(resultsDir, exportName + ".pdy")):
+            continue
+        bamFile = signals['filePath'][j]
+
         # Get dataframe of signal coverage in bed regions, append to dict
         # Make task
-        task = Coverage(peaks, 30, os.path.join(bamFilePath, sample + ".bam"),
-                        orientation=True, fragment_size=1, strand_wise=False, queue="shortq", cpusPerTask=8
+        task = Coverage(peaks, 2, os.path.join(bamFile),
+                        orientation=True, fragment_size=1, strand_wise=True, queue="shortq", cpusPerTask=8
                         )
+        task.id = exportName
+
         slurm.add_task(task)
         slurm.submit(task)
-        while not task.is_ready():
-            time.sleep(10)
-        cov = task.collect()
 
-        # Make multiindex dataframe
-        levels = [cov.keys(), ["+", "-"]]
-        labels = [[y for x in range(len(cov)) for y in [x, x]], [y for x in range(len(cov.keys())) for y in (0, 1)]]
-        index = pd.MultiIndex(labels=labels, levels=levels, names=["peak", "strand"])
-        df = pd.DataFrame(np.vstack(cov.values()), index=index)
-        df.columns = range(windowRange[0], windowRange[1])
+while not all([t.is_ready() for t in slurm.tasks]):
+    time.sleep(10)
 
-        # For strand_wise=False
-        # cov = coverage(bamfile, peaks, fragmentsize)
-        # df = pd.DataFrame(cov).T
-        # df.columns = range(windowRange[0], windowRange[1])
+# Collect task outputs
+for task in slurm.tasks:
+    print task.id
+    if os.path.isfile(os.path.join(resultsDir, task.id + ".pdy")):
+        continue
+    if not task.is_ready():
+        continue
+    cov = task.collect()
 
-        # append to dict
-        rawSignals[sample] = df
+    # Make multiindex dataframe
+    levels = [cov.keys(), ["+", "-"]]
+    labels = [[y for x in range(len(cov)) for y in [x, x]], [y for x in range(len(cov.keys())) for y in (0, 1)]]
+    index = pd.MultiIndex(labels=labels, levels=levels, names=["peak", "strand"])
+    df = pd.DataFrame(np.vstack(cov.values()), index=index)
+    df.columns = range(windowRange[0], windowRange[1])
 
-        # Save as csv
-        df.to_csv(os.path.join(coverageFilePath, "{0}.tssSignal_{1}bp.csv".format(sample, str(windowWidth))), index=False)
+    # Save raw data
+    savePandas(os.path.join(resultsDir, task.id + ".pdy"), df)
+    del cov
+    del df
+    gc.collect()
 
+# Average signals
+aveSignals = dict()
+# aveSignals = pickle.load(open(os.path.join(resultsDir, "aveSignals.pickle"), "r"))
+for i in range(len(sampleSubset)):
+    sampleName = sampleSubset['sampleName'][i]
+
+    signalName = sampleName
+    exportName = "-".join([sampleName, signalName])
+    if exportName in aveSignals.keys():
+        continue
+
+    df = loadPandas(os.path.join(resultsDir, exportName + ".pdy")).copy()
+    # Get average profiles and append to dict
+    ave = {"sample": sampleName,
+           "signal": signalName,
+           "average": pd.Series(smooth(df.apply(np.mean, axis=0))[10:], index=df.columns),                           # both strands
+           "positive": pd.Series(smooth(df.ix[range(0, len(df), 2)].apply(np.mean, axis=0))[10:], index=df.columns), # positive strand
+           "negative": pd.Series(smooth(df.ix[range(1, len(df), 2)].apply(np.mean, axis=0))[10:], index=df.columns)  # negative strand
+           }
+    aveSignals[exportName] = pd.DataFrame(ave)
+    pickle.dump(aveSignals, open(os.path.join(resultsDir, "aveSignals.pickle"), "wb"), protocol=pickle.HIGHEST_PROTOCOL)
+
+    for j in range(len(signals)):
+        signalName = signals['sampleName'][j]
+
+        exportName = "-".join([sampleName, signalName])
+
+        if exportName in aveSignals.keys():
+            continue
+
+        df = loadPandas(os.path.join(resultsDir, exportName + ".pdy")).copy()
         # Get average profiles and append to dict
-        ave = {"signal": sample,
-               "average": df.apply(np.mean, axis=0),                              # both strands
-               "positive": df.ix[range(0, len(df), 2)].apply(np.mean, axis=0),    # positive strand
-               "negative": df.ix[range(1, len(df), 2)].apply(np.mean, axis=0)     # negative strand
+        ave = {"sample": sampleName,
+               "signal": signalName,
+               "average": pd.Series(smooth(df.apply(np.mean, axis=0))[10:], index=df.columns),                           # both strands
+               "positive": pd.Series(smooth(df.ix[range(0, len(df), 2)].apply(np.mean, axis=0))[10:], index=df.columns), # positive strand
+               "negative": pd.Series(smooth(df.ix[range(1, len(df), 2)].apply(np.mean, axis=0))[10:], index=df.columns)  # negative strand
                }
-        aveSignals[sample] = pd.DataFrame(ave)
+        aveSignals[exportName] = pd.DataFrame(ave)
 
-# serialize with pickle
-with open(os.path.join(coverageFilePath, "tssSignals_{0}bp.rawSignals.pickle".format(str(windowWidth))), 'wb') as handle:
-    pickle.dump(rawSignals, handle, protocol=pickle.HIGHEST_PROTOCOL)
-# with open(os.path.join(coverageFilePath, "tssSignals_{0}bp.rawSignals.pickle".format(str(windowWidth))), 'rb') as handle:
-#     rawSignals = pickle.load(handle)
-
-with open(os.path.join(coverageFilePath, "tssSignals_{0}bp.aveSignals.pickle".format(str(windowWidth))), 'wb') as handle:
-    pickle.dump(aveSignals, handle, protocol=pickle.HIGHEST_PROTOCOL)
-# with open(os.path.join(coverageFilePath, "tssSignals_{0}bp.aveSignals.pickle".format(str(windowWidth))), 'rb') as handle:
-#     aveSignals = pickle.load(handle)
+        pickle.dump(aveSignals, open(os.path.join(resultsDir, "aveSignals.pickle"), "wb"), protocol=pickle.HIGHEST_PROTOCOL)
 
 # Plot average profiles for all signals with ggplot
 aveSignals = pd.concat(aveSignals, axis=0)
 aveSignals["x"] = [i[1] for i in aveSignals.index]
-aveSignals = pd.melt(aveSignals, id_vars=["x", "signal"])
+aveSignals = pd.melt(aveSignals, id_vars=["x", "sample", "signal"])
 
 plotFunc = robj.r("""
     library(ggplot2)
 
-    #cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
-
-    function(df, plotsDir){
+    function(df, width, plotsDir){
         p = ggplot(df, aes(x, value, colour = variable)) +
             geom_line() +
-            # stat_smooth(method = "gam", formula = y ~ s(x, k = 20), se = FALSE) +
-            facet_wrap(~signal, scales="free", ncol=1) +
+            #stat_smooth(se = F, method = "lm", formula = y ~ poly(x, 24)) +
+            #stat_smooth(method = "gam", formula = y ~ s(x, k = 80), se = FALSE) +
+            #stat_smooth(method = "loess", formula = y ~ x, size = 1, se = FALSE) +
+            facet_wrap(sample~signal, scales="free", ncol=9) +
             xlab("Distance to peak") +
             ylab("Average tags per bp") +
             theme_bw() +
             theme(legend.title=element_blank()) +
-            #scale_colour_manual(values=cbPalette[c("grey", ,3,7)])
             scale_colour_manual(values=c("black", "dark blue","dark red")) +
-            xlim(c(-200, 200))
+            xlim(c(-width, width))
 
-        ggsave(filename = paste0(plotsDir, "/tss_signal.pdf"), plot = p, height = 7, width = 7)
-        ggsave(filename = paste0(plotsDir, "/tss_signal.wide.pdf"), plot = p, height = 7, width = 10)
+        ggsave(filename = paste0(plotsDir, "/TFs_signal.", width * 2, ".pdf"), plot = p, height = 20, width = 40)
+        ggsave(filename = paste0(plotsDir, "/TFs_signal.", width * 2, ".wide.pdf"), plot = p, height = 20, width = 60, limitsize=FALSE)
     }
 """)
 
@@ -307,7 +489,21 @@ robj.pandas2ri.activate()
 aveSignals_R = robj.conversion.py2ri(aveSignals)
 
 # run the plot function on the dataframe
-plotFunc(aveSignals_R, plotsDir)
+plotFunc(aveSignals_R, 50, plotsDir)
+plotFunc(aveSignals_R, 100, plotsDir)
+plotFunc(aveSignals_R, 200, plotsDir)
+plotFunc(aveSignals_R, 500, plotsDir)
+
+
+
+
+
+
+
+
+
+
+# Clustering
 
 # Loop through raw signals, normalize, k-means cluster, save pickle, plot heatmap, export cdt
 for signal in signals:
@@ -345,6 +541,15 @@ for signal in signals:
 
     # Export as cdt
     exportToJavaTreeView(df, os.path.join(plotsDir, exportName + ".cdt"))
+
+
+
+
+
+
+
+
+
 
 
 """
