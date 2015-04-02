5c9c42bd1900f0bc2f1434805c122c48f1d45dcd
André Rendeiro <afrendeiro@gmail.com>
2015-04-02 21:16:41 +0200

    correlation plots for all samples and comparing different cells/techniques

--- src/correlation_analysis.py
+++ src/correlation_analysis.py
@@ -9,199 +9,272 @@
 #############################################################################################
 
 import os
-from pybedtools import BedTool
-import HTSeq
-import string
+import re
 import numpy as np
 import pandas as pd
+import itertools
 import rpy2.robjects as robj # for ggplot in R
 import rpy2.robjects.pandas2ri # for R dataframe conversion
 
-# Define variables
-signals = [
-    "H3K4me3_K562_500k_CM", "H3K4me3_K562_500k_ChIP", "H3K4me3_K562_10k_CM",
-    "H3K27me3_K562_500k_CM", "H3K27me3_K562_500k_ChIP", "H3K27me3_K562_10k_CM",
-]
-bamFilePath = "/home/arendeiro/data/human/chipmentation/mapped/merged/"
-genomeSizes = "/fhgfs/prod/ngs_resources/genomes/hg19/hg19_chromLengths_sorted.txt"
-plotsDir = "home/arendeiro/projects/chipmentation/results/plots"
-
-genome = "hg19"
-windowWidth = 1000
-fragmentsize = 1
-duplicates = False
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
-def coverage(bam, intervals, fragmentsize, duplicates=False):
-    """ Gets read coverage in bed regions, returns dict with region:count.
-    bam - Bam object from HTSeq.BAM_Reader.
-    intervals - dict with HTSeq.GenomicInterval objects as values.
-    fragmentsize - integer.
-    duplicates - boolean.
-    """
-    chroms = ['chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chr20', 'chr21', 'chr22', 'chrX']
-    # Loop through TSSs, get coverage, append to dict
-    cov = dict()
+def getCounts(sampleSubset):
+    normCounts = pd.DataFrame()
 
-    for name, feature in intervals.iteritems():
-        if feature.chrom not in chroms:
+    for sample in range(len(sampleSubset)):
+        name = sampleSubset.ix[sample]["sampleName"]
+
+        # read in bed file with coverage
+        try:
+            df = pd.read_csv(os.path.join(coverageDir, name + ".cov"), sep="\t", header=None)
+        except:
             continue
-        count = 0
 
-        # Fetch alignments in feature window
-        for aln in bam[feature]:
-            # check if duplicate
-            if not duplicates and aln.pcr_or_optical_duplicate:
-                continue
+        # get series with counts
+        counts = df[4]
 
-            # adjust fragment to size
-            aln.iv.length = fragmentsize
+        # normalize by total size
+        norm = np.log2(1 + (counts / sum(counts)) * 1000000)
 
-            # add +1 to all positions overlapped by read within window
-            count += 1
-
-        # append feature profile to dict
-        cov[name] = count
-    return cov
+        # append to raw counts
+        name = re.sub("_hg19", "", name)
+        name = re.sub("K562_", "", name)
+        normCounts[name] = norm
+    return normCounts
 
 
-# Generate 1kb windows genome-wide
-w = BedTool.window_maker(BedTool(), genome=genomeSizes, w=windowWidth)
-windows = dict()
-for interval in w:
-    feature = HTSeq.GenomicInterval(
-        interval.chrom,
-        interval.start,
-        interval.end
-    )
-    name = string.join(interval.fields, sep="_")
-    windows[name] = feature
+def colourPerFactor(name):
+    name = str(name.upper())
+    if "H3K4ME3" in name:
+        return "#009e73"
+    elif "H3K4ME1" in name:
+        return "#e69f00"
+    elif "H3K27AC" in name:
+        return "#D55E29"
+    elif "H3K27ME3" in name:
+        return "#0072b2"
+    elif "H3K36ME3" in name:
+        return "#9e2400"
+    elif "CTCF" in name:
+        return "#534202"
+    elif "PU1" in name:
+        return "#6E022C"
+    elif "GATA1" in name:
+        return "#9b0505"
+    elif "GATA2" in name:
+        return "#510303"
+    elif "REST" in name:
+        return "#25026d"
+    elif "CJUN" in name:
+        return "#2a0351"
+    elif "FLI1" in name:
+        return "#515103"
+    elif "IGG" in name:
+        return "#d3d3d3"
+    elif "INPUT" in name:
+        return "#d3d3d3"
+    elif "NAN_NAN_NAN" in name:  # DNAse, ATAC
+        return "#00523b"
+    else:
+        raise ValueError
 
-# Loop through all signals, compute coverage in bed regions, append to dict
-rawSignals = dict()
-for signal in signals:
-    # Load bam
-    bamfile = HTSeq.BAM_Reader(os.path.join(bamFilePath, signal + ".bam"))
 
-    # Get dataframe of signal coverage in bed regions, append to dict
-    rawSignals[signal] = coverage(bamfile, windows, fragmentsize, duplicates)
+def plotCorrelations(normCounts, plotName):
+    colours = map(colourPerFactor, normCounts.columns)
+    cmap = sns.diverging_palette(h_neg=210, h_pos=350, s=90, l=30, as_cmap=True)
 
-df = pd.DataFrame(rawSignals)
+    sns.clustermap(normCounts.corr(), row_colors=colours, method="ward",
+                   col_colors=colours, figsize=(15, 15), cmap=cmap)
 
-# Normalize to library size
-dfNorm = df.apply(lambda x: np.log2(1 + (x / x.sum()) * 1000000))
+    plt.savefig(plotName, bbox_inches='tight')
 
-# Plot with R
-plotFunc = robj.r("""
+
+def plotScatterCorrelation(df, path):
+    x = df.columns[0]
+    y = df.columns[1]
+
+    # Plot with R
+    plotFunc = robj.r("""
     library(ggplot2)
-    library(reshape2)
 
-    function(df, plotsDir){
+    function(df, path, x, y) {
+
+        pdf(path, height = 12, width = 12)
+
         # scatterplot
-        pdf(paste(projectDir, "results/plots/correlations_1kb_windows_scatter_mergedReplicates.pdf", sep = ""), height = 11, width = 7)
-        m = matrix(c(1,2,3,4,5,5,6,7) ,nrow = 4,ncol = 2, byrow = TRUE)
-        layout(mat = m, heights = c(1,1,0.2,1))
-
-        par(oma = c(5,5,2,0) + 0.1,
-            mar = c(0,0,0,0) + 0.1,
-            pty = "s")
-
-        smoothScatter(log2(rawCounts$H3K4me3_K562_500k_CM), log2(rawCounts$H3K4me3_K562_500k_ChIP),
-            col = rgb(104,104,104,50 , maxColorValue = 255), pch = 16, nrpoints = 0, ann = FALSE, xaxt = 'n')
-        text(par('usr')[1] + 1.8, par('usr')[4] - 0.5,
-            bquote(R^2 == .(round(cor(rawCounts$H3K4me3_K562_500k_CM, rawCounts$H3K4me3_K562_500k_ChIP), 3))),
+        smoothScatter(
+            log2(df$A), log2(df$B),
+            col = rgb(104,104,104,50, maxColorValue = 255),
+            pch = 16,
+            xlab = x,
+            ylab = y,
+            nrpoints = 0,
+            ann = FALSE#,
+            #xaxt = 'n'
+        )
+        # pearson's R as text
+        text(
+            par('usr')[1] + 1.8,
+            par('usr')[4] - 0.5,
+            bquote(R^2 == .(round(cor(df$A, df$B), 3))),
             cex = 1.6
         )
-        mtext(side = 2, "500.000 cells", line = 2)
-        mtext(side = 3, "H3K4me3")
-
-        smoothScatter(log2(rawCounts$H3K27me3_K562_500k_CM), log2(rawCounts$H3K27me3_K562_500k_ChIP),
-            col = rgb(104,104,104,50 , maxColorValue = 255), pch = 16, nrpoints = 0, ann = FALSE, xaxt = 'n', yaxt = 'n')
-        text(par('usr')[1] + 1.8, par('usr')[4] - 0.5,
-            bquote(R^2 == .(round(cor(rawCounts$H3K27me3_K562_500k_CM, rawCounts$H3K27me3_K562_500k_ChIP), 3))),
-            cex = 1.6
-        )
-        mtext(side = 3, "H3K27me3")
-
-        smoothScatter(log2(rawCounts$H3K4me3_K562_10k_CM), log2(rawCounts$H3K4me3_K562_500k_ChIP),
-            col = rgb(104,104,104,50 , maxColorValue = 255), pch = 16, nrpoints = 0, ann = FALSE)
-        mtext(side = 2, "10.000 cells", line = 2)
-        text(par('usr')[1] + 1.8, par('usr')[4] - 0.5,
-            bquote(R^2 == .(round(cor(rawCounts$H3K4me3_K562_10k_CM, rawCounts$H3K4me3_K562_500k_ChIP), 3))),
-            cex = 1.6
-        )
-
-        smoothScatter(log2(rawCounts$H3K27me3_K562_10k_CM), log2(rawCounts$H3K27me3_K562_500k_ChIP),
-            col = rgb(104,104,104,50 , maxColorValue = 255), pch = 16, nrpoints = 0, ann = FALSE, yaxt = 'n')
-        text(par('usr')[1] + 1.8, par('usr')[4] - 0.5,
-            bquote(R^2 == .(round(cor(rawCounts$H3K27me3_K562_10k_CM, rawCounts$H3K27me3_K562_500k_ChIP), 3))),
-            cex = 1.6
-        )
-        mtext(side = 1, "ChIP", outer= FALSE, at = -6.2, cex = 1, line = 3)
-
-        plot.new()
-        #mtext(side = 1, "ChIP", line = 0, at = 0)
-
-        smoothScatter(log2(rawCounts$H3K4me3_K562_500k_CM), log2(rawCounts$H3K4me3_K562_10k_CM),
-            col = rgb(104,104,104,50 , maxColorValue = 255), pch = 16, nrpoints = 0, ann = FALSE)
-        mtext(side = 2, "500.000 cells", line = 2)
-        text(par('usr')[1] + 1.8, par('usr')[4] - 0.5,
-            bquote(R^2 == .(round(cor(rawCounts$H3K4me3_K562_500k_CM, rawCounts$H3K4me3_K562_10k_CM), 3))),
-            cex = 1.6
-        )
-        mtext(side = 1, "10.000 cells", line = 2, cex.lab = 1.5)
-
-        smoothScatter(log2(rawCounts$H3K27me3_K562_500k_CM), log2(rawCounts$H3K27me3_K562_10k_CM),
-            col = rgb(104,104,104,50 , maxColorValue = 255), pch = 16, nrpoints = 0, ann = FALSE, yaxt = 'n')
-        text(par('usr')[1] + 1.8, par('usr')[4] - 0.5,
-            bquote(R^2 == .(round(cor(rawCounts$H3K27me3_K562_500k_CM, rawCounts$H3K27me3_K562_10k_CM), 3))),
-            cex = 1.6
-        )
-        mtext(side = 1, "10.000 cells", line = 2, cex.lab = 1.5)
-
-        title(xlab = "ChIPmentation",
-              ylab = "ChIPmentation",
-              outer = TRUE, cex.lab = 1.5)
         dev.off()
     }
-""")
+    """)
+    df.columns = ["A", "B"]
 
-# convert the pandas dataframe to an R dataframe
-robj.pandas2ri.activate()
-df_R = robj.conversion.py2ri(dfNorm)
+    # convert the pandas dataframe to an R dataframe
+    robj.pandas2ri.activate()
+    df_R = robj.conversion.py2ri(df)
 
-# run the plot function on the dataframe
-plotFunc(df_R, plotsDir)
-
-# TODO: implement multipleCorrelations function
-# def multipleCorrelations(dataframe):
-#     """Calculates the Pearson correlation between all columns in a pandas dataframe.
-#     """
-#     raise NotImplemented
-#
-# cor = multipleCorrelations(df)
-
-# plotFunc = robj.r("""
-#     library(ggplot2)
-#     library(reshape2)
-
-#     function(df, plotsDir){
-#         # scatterplot
-#         pdf(paste0(plotsDir, "/correlations_1kb_windows.pdf", sep = ""), height = 11, width = 7)
-
-#         smoothScatter(log2(df$H3K4me3), log2(df$H3K4me2),
-#             col = rgb(104,104,104,50 , maxColorValue = 255), pch = 16, nrpoints = 0, ann = FALSE, xaxt = 'n')
-#         text(par('usr')[1] + 1.8, par('usr')[4] - 0.5,
-#             bquote(R^2 == .(round(cor(df$H3K4me3, df$H3K4me2), 3))),
-#             cex = 1.6
-#         )
-#         mtext(side = 2, "500.000 cells", line = 2)
-#         mtext(side = 3, "H3K4me3")
+    # run the plot function on the dataframe
+    plotFunc(df_R, path, x, y)
 
 
-#         title(xlab = "ChIPmentation",
-#               ylab = "ChIPmentation",
-#               outer = TRUE, cex.lab = 1.5)
-#         dev.off()
-#     }
-# """)
+def plotKDE(s1, s2, path):
+    sns.jointplot(s1, s2, kind="kde", size=7, space=0)
+    plt.savefig(path, bbox_inches='tight')
+
+
+# Define paths
+projectRoot = "/fhgfs/groups/lab_bock/shared/projects/chipmentation/"
+coverageDir = projectRoot + "data/coverage"
+resultsDir = projectRoot + "results"
+plotsDir = resultsDir + "/plots"
+
+# Get samples
+samples = pd.read_csv(os.path.abspath(projectRoot + "chipmentation.replicates.annotation_sheet.csv"))
+
+
+# All
+# subset samples
+sampleSubset = samples.reset_index(drop=True)
+sampleSubset = sampleSubset.sort(["ip", "technique"]).reset_index(drop=True)
+
+normCounts = getCounts(sampleSubset)
+plotCorrelations(normCounts, os.path.join(plotsDir, "correlations.all.pdf"))
+
+
+# All final
+# subset samples
+sampleSubset = samples[
+    (samples["technique"].str.contains("CM|CHIP")) &
+    (samples["ip"].str.contains("H3K4ME3|H3K4ME1|H3K27ME3|H3K36ME3|H3K27AC|CTCF|PU1|GATA1|REST")) &
+    (samples["numberCells"].str.contains("10M|500K|100K|10K")) &
+    (samples["technicalReplicate"] == 0) &
+    (samples["biologicalReplicate"] != 0)
+].reset_index(drop=True)
+sampleSubset = sampleSubset.sort(["ip", "technique"]).reset_index(drop=True)
+
+normCounts = getCounts(sampleSubset)
+plotCorrelations(normCounts, os.path.join(plotsDir, "correlations.set.pdf"))
+
+# Histones
+# subset samples
+sampleSubset = samples[
+    (samples["technique"].str.contains("CM|CHIP")) &
+    (samples["ip"].str.contains("H3K4ME3|H3K4ME1|H3K27ME3|H3K36ME3|H3K27AC")) &
+    (samples["numberCells"].str.contains("10M|500K|10K"))  # &
+    # (samples["biologicalReplicate"] == 0)
+].reset_index(drop=True)
+sampleSubset = sampleSubset.sort(["ip", "technique"]).reset_index(drop=True)
+
+normCounts = getCounts(sampleSubset)
+plotCorrelations(normCounts, os.path.join(plotsDir, "correlations.histones.pdf"))
+
+
+# TFs
+# subset samples
+sampleSubset = samples[
+    (samples["technique"].str.contains("CM|CHIP")) &
+    (samples["ip"].str.contains("CTCF|PU1|GATA1|REST")) &
+    (samples["numberCells"].str.contains("10M|500K|100K|10K"))
+].reset_index(drop=True)
+sampleSubset = sampleSubset.sort(["ip", "technique"]).reset_index(drop=True)
+
+normCounts = getCounts(sampleSubset)
+plotCorrelations(normCounts, os.path.join(plotsDir, "correlations.TFs.pdf"))
+
+
+# Figure 1:
+#   correlations between biological == 0 for each factor
+#   in combinations of #cells and technique
+
+for IP in samples['ip'].unique():
+    subset = samples[samples["ip"] == IP]
+
+    cells = subset['numberCells'].unique()
+    tec = subset['technique'].unique()
+
+    for t in subset['technique'].unique():
+        for (v1, v2) in itertools.combinations(cells, 2):
+            c1 = samples[
+                (samples["ip"] == IP) &
+                (samples["numberCells"] == v1) &
+                (samples["technique"] == t) &
+                (samples["biologicalReplicate"] == 0)
+            ]
+            c2 = samples[
+                (samples["ip"] == IP) &
+                (samples["numberCells"] == v2) &
+                (samples["technique"] == t) &
+                (samples["biologicalReplicate"] == 0)
+            ]
+            if c1.empty or c2.empty:
+                continue
+
+            normCounts = getCounts(c1.append(c2).reset_index())
+            normCounts.columns = c1.sampleName.tolist() + c2.sampleName.tolist()
+
+            plotScatterCorrelation(
+                normCounts,
+                os.path.join(
+                    plotsDir,
+                    "correlation.{0}.{1}.{2}_vs_{3}.pdf".format(IP, t, v1, v2)
+                )
+            )
+            # plotKDE(
+            #     normCounts.icol(0),
+            #     normCounts.icol(1),
+            #     os.path.join(
+            #         plotsDir,
+            #         "correlation.{0}.{1}.{2}_vs_{3}.pdf".format(IP, t, v1, v2)
+            #     )
+            # )
+
+for IP in samples['ip'].unique():
+    subset = samples[samples["ip"] == IP]
+
+    cells = subset['numberCells'].unique()
+    tec = subset['technique'].unique()
+
+    for c in subset['numberCells'].unique():
+        for (v1, v2) in itertools.combinations(tec, 2):
+            t1 = samples[
+                (samples["ip"] == IP) &
+                (samples["numberCells"] == c) &
+                (samples["technique"] == v1) &
+                (samples["biologicalReplicate"] == 0)
+            ]
+            t2 = samples[
+                (samples["ip"] == IP) &
+                (samples["numberCells"] == c) &
+                (samples["technique"] == v2) &
+                (samples["biologicalReplicate"] == 0)
+            ]
+            if t1.empty or t2.empty:
+                continue
+
+            normCounts = getCounts(t1.append(t2).reset_index())
+            normCounts.columns = t1.sampleName.tolist() + t2.sampleName.tolist()
+
+            plotScatterCorrelation(
+                pd.DataFrame(t1, t2),
+                os.path.join(
+                    plotsDir,
+                    "correlation.{0}.{1}.{2}_vs_{3}.pdf".format(IP, c, v1, v2)
+                )
+            )
