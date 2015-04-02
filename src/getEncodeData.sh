af00a8421f2ab56715037b89ca43e381343b77ec
André Rendeiro <afrendeiro@gmail.com>
2015-04-02 12:45:10 +0200

    added new encode data to analysis and to sample annotation

---
+++ src/getEncodeData.sh
@@ -0,0 +1,39 @@
+#!/usr/env bash
+
+getBamFile () {
+    SAMPLE=$1
+    BAMFOLDER=/fhgfs/groups/lab_bock/shared/projects/chipmentation/data/mapped/
+    URL=hgdownload.cse.ucsc.edu/goldenPath/hg19/encodeDCC/wgEncodeBroadHistone/
+
+    rsync -a -P rsync://${URL}/$SAMPLE.bam $BAMFOLDER
+    rsync -a -P rsync://${URL}/$SAMPLE.bam.bai $BAMFOLDER
+}
+export -f getBamFile
+
+mergeBams () {
+    SAMPLE=$1
+    BAMFOLDER=/fhgfs/groups/lab_bock/shared/projects/chipmentation/data/mapped
+
+    samtools merge ${BAMFOLDER}/${SAMPLE}.bam ${BAMFOLDER}/${SAMPLE}Rep1.bam ${BAMFOLDER}/${SAMPLE}Rep2.bam
+    samtools index ${BAMFOLDER}/${SAMPLE}.bam
+}
+export -f mergeBams
+
+# getPeakFile () {
+#     SAMPLE=$1
+#     BAMFOLDER=/fhgfs/groups/lab_bock/shared/projects/chipmentation/data/mapped/
+#     URL=hgdownload.cse.ucsc.edu/goldenPath/hg19/encodeDCC/wgEncodeBroadHistone/
+
+#     rsync -a -P rsync://${URL}/$SAMPLE.bam $BAMFOLDER
+#     rsync -a -P rsync://${URL}/$SAMPLE.bam.bai $BAMFOLDER
+# }
+# export -f getPeakFile
+
+module load parallel
+parallel getBamFile ::: {wgEncodeBroadHistoneK562H3k4me1StdAln,wgEncodeBroadHistoneK562H3k27me3StdAln,wgEncodeBroadHistoneK562H3k36me3StdAln,wgEncodeBroadHistoneK562H3k4me3StdAln}Rep{1,2}
+parallel mergeBams ::: wgEncodeBroadHistoneK562H3k4me1StdAln wgEncodeBroadHistoneK562H3k27me3StdAln wgEncodeBroadHistoneK562H3k36me3StdAln wgEncodeBroadHistoneK562H3k4me3StdAln
+#parallel getPeakFile ::: {wgEncodeBroadHistoneK562H3k4me1StdAln,wgEncodeBroadHistoneK562H3k27me3StdAln,wgEncodeBroadHistoneK562H3k36me3StdAln,wgEncodeBroadHistoneK562H3k4me3StdAln}Rep{1,2}
+
+# Input
+getBamFile wgEncodeBroadHistoneK562ControlStdAlnRep1
+
