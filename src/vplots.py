c873850eeed6a9a87a172c9ec72373bfccc8df65
André Rendeiro <afrendeiro@gmail.com>
2015-04-29 21:35:35 +0200

    changed scale of vplots

--- src/vplots.py
+++ src/vplots.py
@@ -5,15 +5,16 @@ import seaborn as sns
 
 sns.set_style("white")
 
-df = pd.read_csv("dyads_vplot.tsv", sep="\t", header=None)
+df = pd.read_csv("/media/afr/cemm-backup/chipmentation/data/nucleoATAC/10M_CM_H3K4ME1_PE/dyads_vplot.tsv", sep="\t", header=None)
 df = df.sort(ascending=False)
 df.columns = np.array(range(len(df.columns))) - (len(df.columns) / 2)
 
 df = np.log2(1 + df)
 
-cmap = plt.get_cmap("BuGn")
+# cmap = plt.get_cmap("BuGn")
+cmap = plt.get_cmap("pink_r")
 plt.imshow(df, cmap=cmap)
-plt.yticks(plt.yticks()[0][2:-1], plt.yticks()[0][2:-1][::-1])
+plt.yticks(plt.yticks()[0][2:-1], [200, 150, 100, 50])
 plt.xticks(plt.xticks()[0][1:-1], [-200, -100, 0, 100, 200])
 
 plt.vlines(200 - 60, 0, 250, linestyles="--")
@@ -23,7 +24,7 @@ plt.colorbar()
 plt.xlabel("distance to dyad (bp)")
 plt.ylabel("fragment size (bp)")
 
-plt.savefig("dyads_vplot.pdf", bbox_inches='tight')
+plt.savefig("dyads_vplot2.pdf", bbox_inches='tight')
 plt.close()
 
 # Zoom on nucleosomal part
@@ -31,17 +32,18 @@ plt.imshow(df.loc[250:105, -60:60], cmap=cmap)
 plt.colorbar()
 plt.xlabel("distance to dyad (bp)")
 plt.ylabel("fragment size (bp)")
-
-plt.savefig("dyads_vplot.zoom.pdf", bbox_inches='tight')
+plt.xticks([0, 30, 60, 90, 120], [-60, -30, 0, 30, 60])
+plt.yticks([0, 50, 100, 140], [250, 200, 150, 110])
+plt.savefig("dyads_vplot.zoom2.pdf", bbox_inches='tight')
 plt.close()
 
 
 # Insertion frequency
 sns.set_style("whitegrid")
-df = pd.read_csv("10M_CM_H3K4ME1_PE.vplot.InsertionProfile.txt", header=None)
+df = pd.read_csv("/media/afr/cemm-backup/chipmentation/data/nucleoATAC/10M_CM_H3K4ME1_PE/10M_CM_H3K4ME1_PE.vplot.InsertionProfile.txt", header=None)
 df["x"] = range(-325, 326)
 
-plt.plot(df["x"], df[0])
+plt.plot(df["x"], df[0], color="#e69f00")
 
 plt.xlabel("distance to dyad (bp)")
 plt.ylabel("insertion frequency")
