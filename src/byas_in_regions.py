b00c25e05c1f9ed5058a087947e1dff778303244
André Rendeiro <afrendeiro@gmail.com>
2015-05-09 23:03:02 +0200

    memory-friendly now; savegames as well

--- src/byas_in_regions.py
+++ src/byas_in_regions.py
@@ -35,31 +35,70 @@ PWM = [
 
 # Calculate matches to PWM
 D = dict()
-for region in ["ctcf", "gata1", "pu1", "rest", "tss"]:
+DF = pd.DataFrame(columns=["sample", "signal", "average", "x", "type"])
+
+for region in ["K562_10M_CM_CTCF_nan_nan_0_0_hg19",
+               "K562_10M_CM_GATA1_nan_nan_0_0_hg19",
+               "K562_10M_CM_PU1_nan_nan_0_0_hg19",
+               "K562_10M_CM_REST_nan_nan_0_0_hg19",
+               "tss"]:
+    print(region)
     if region != "tss":
         bed = pybedtools.BedTool(
             "/media/afr/cemm-backup/chipmentation/data/peaks/" +
-            "K562_10M_CM_{0}_nan_nan_0_0_hg19/K562_10M_CM_{0}_nan_nan_0_0_hg19_peaks.motifCentered.bed".format(region.upper()))
+            "{0}/{0}_peaks.motifCentered.bed".format(region))
     else:
         bed = pybedtools.BedTool("/media/afr/cemm-backup/hg19.cage_peak_coord_robust.TATA_Annotated.bed")
+        bed = bed.slop(b=200, genome="hg19")
 
     # Get nucleotides
     seq = bed.sequence(s=True, fi=fasta)
     seqs = open(seq.seqfn).read().split("\n")[1::2]  # get fasta sequences
 
     # Match strings with PWM
-    scores = list()
+    scores = np.array(range(len(seqs[0]) - 20))
     for sequence in seqs:
         result = MOODS.search(sequence, [PWM], 30)
-        scores.append([j for i, j in result[0]])
-    D[region] = pd.DataFrame(scores)
+        scores += np.array([j for i, j in result[0]])
+    D[region] = scores / float(len(seqs))
+    if region != "tss":
+        DFF = pd.DataFrame([scores / float(len(seqs)), range(1980)]).T
+    else:
+        DFF = pd.DataFrame([scores / float(len(seqs)), range(380)]).T
+    DFF.columns = ["average", "x"]
+    DFF["sample"] = region
+    DFF["signal"] = "pwm"
+    DFF["type"] = "raw"
+    DF = DF.append(DFF).reset_index(drop=True)
 
-# average
-d = map(lambda x: x.apply(np.mean, axis=0), D.values())
-d = d.drop(0)
-d = d.T
-d.columns = ['rest', 'ctcf', 'pu1', 'gata1']
-pickle.dump(d, open("tf_bias.pickle", "wb"), protocol=pickle.HIGHEST_PROTOCOL)
+# save
+pickle.dump(D, open("/media/afr/cemm-backup/chipmentation/tf_bias.pickle", "wb"), protocol=pickle.HIGHEST_PROTOCOL)
+D = pickle.load(open("/media/afr/cemm-backup/chipmentation/tf_bias.pickle", "r"))
+
+pickle.dump(DF, open("/media/afr/cemm-backup/chipmentation/tf_bias.df.pickle", "wb"), protocol=pickle.HIGHEST_PROTOCOL)
+DF = pickle.load(open("/media/afr/cemm-backup/chipmentation/tf_bias.df.pickle", "r"))
 
 # Plot
-d.plot()
+for region, data in D.items():
+    plt.plot(data[1000 - 380:1000 + 380], label=region)  # plot center 800 bp
+plt.legend()
+plt.show()
+
+
+# PWMs example
+from Bio import Seq, Alphabet, motifs
+a = Alphabet.IUPAC.IUPACUnambiguousDNA
+seqs = [
+    Seq.Seq("ACTG", alphabet=a),
+    Seq.Seq("ACTG", alphabet=a),
+    Seq.Seq("ACTG", alphabet=a),
+    Seq.Seq("ACTG", alphabet=a),
+    Seq.Seq("ACTC", alphabet=a),
+    Seq.Seq("ACTT", alphabet=a),
+    Seq.Seq("ACTC", alphabet=a),
+    Seq.Seq("ACTT", alphabet=a),
+    Seq.Seq("ACTA", alphabet=a)
+]
+m = motifs.create(seqs, alphabet=Alphabet.IUPAC.IUPACUnambiguousDNA)
+m.pwm
+m.weblogo("logo.png")
